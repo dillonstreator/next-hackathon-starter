0f175ad0062525420f78764365ecf1b1409415ee
dillon <dillonstreator@gmail.com>
2020-05-17 22:13:49 -0500

    add default first param for validate-checks middleware

--- middlewares/validate-checks.js
+++ middlewares/validate-checks.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 const onErrorsDefault = ({ req, res, next, errors }) =>
 	res.status(400).json({ errors: errors.array() });
 
-module.exports = ({ onErrors = onErrorsDefault }) => (req, res, next) => {
+module.exports = ({ onErrors = onErrorsDefault } = {}) => (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return onErrors({ req, res, next, errors });
